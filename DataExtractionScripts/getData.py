da5d7c9ef23585bccae4681853b4dd1bc831cb26
Raghavan S <sraghavan02@gmail.com>
2020-07-27 15:14:14 -0700

    command line args for extraction

--- DataExtractionScripts/getData.py
+++ DataExtractionScripts/getData.py
@@ -2,11 +2,14 @@ import requests
 import gzip
 import os
 import json
+import sys
 if not os.path.exists('Datasets'):
     os.makedirs('Datasets')
-url = 'https://data.gharchive.org/2011-02-12-3.json.gz'
-filename = "Datasets/dataset_sample.json.gz"
-file_Dataset_Unzipped = "Datasets/dataset_sample.json"
+timestamp = sys.argv[1]
+# refer https://www.gharchive.org/ for acceptable timestamp inputs
+url = 'https://data.gharchive.org/' + timestamp + '.json.gz'
+filename = "Datasets/" + timestamp + ".json.gz"
+file_Dataset_Unzipped = "Datasets/" + timestamp + ".json"
 r = requests.get(url)
 with open(filename, 'wb') as f:
     f.write(r.content)
